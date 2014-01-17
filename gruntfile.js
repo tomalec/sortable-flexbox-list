facf458f699fabe277ec695fa260ae1bbd56ca63
tomalec <tomalecpub@gmail.com>
2014-01-17 15:29:36 +0100

    grunt, live-reload

---
+++ gruntfile.js
@@ -0,0 +1,82 @@
+'use strict';
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
+var mountFolder = function (connect, dir) {
+    return connect.static(require('path').resolve(dir));
+};
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+    // show elapsed time at the end
+    require('time-grunt')(grunt);
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
+
+
+    grunt.initConfig({
+        watch: {
+            options: {
+                nospawn: true,
+                livereload: true
+            },
+        
+            livereload: {
+                options: {
+                    livereload: LIVERELOAD_PORT
+                },
+                files: [
+                    '*.html',
+                    'elements/**/*.html',
+                    '{.tmp,.}/styles/{,*/}*.css',
+                    '{.tmp,.}/scripts/{,*/}*.js',
+                    'images/{,*/}*.{png,jpg,jpeg,gif,webp}'
+                ]
+            }
+        },
+        connect: {
+            options: {
+                port: 9001,
+                // change this to '0.0.0.0' to access the server from outside
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            lrSnippet,
+                            mountFolder(connect, '.tmp'),
+                            mountFolder(connect, '.')
+                        ];
+                    }
+                }
+            }
+        },
+        open: {
+            server: {
+                path: 'http://localhost:<%= connect.options.port %>'
+            }
+        },
+        clean: {
+            server: '.tmp'
+        }
+    });
+
+    grunt.registerTask('server', function (target) {
+        if (target === 'dist') {
+            return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
+        }
+
+        grunt.task.run([
+            'clean:server',
+            
+            'connect:livereload',
+            'open',
+            'watch'
+        ]);
+    });
+};
